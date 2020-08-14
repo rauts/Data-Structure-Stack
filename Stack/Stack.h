245f5d288d5a59ccb9b982601ebabba29e80bd8f
Sunil Raut <sunil.rautkshetri@gmail.com>
2020-08-14 10:26:48 +0200

    Add Pop() method in Stack.h

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,37 +1,85 @@
 #pragma once
 #include <iostream>
 using std::cout;
+#include "Array.h"
+#include "StackOverFlowExecption.h"
 
+template <class T>
 class Stack
 {
-	
+
 private:
-	int mMaxStackSize{0};
-	int mCurrentNumberOfItem{ 0 };
-	int* mTop{ nullptr };
+	Array <T> mStack;  // stack elements are stored in the custum defined arrray
+	int mTop{ -1 }; // indicates the current top position in the stack
 
 public:
 	Stack() = delete;
 
-	Stack(int maxStackSize);
+	Stack(int maxStackSize) : mStack{ maxStackSize } {}
 
-	~Stack();
+	~Stack() {}
 
-	void Push(int element);
+	int CurrentSize() const{
+		return mTop + 1;
+	}
 
-	void Pop();
+	int MaxSize() const {
+		return mStack.Size();
+	}
 
-	int Top();
+	bool IsEmpty() const {
+		return CurrentSize == 0;
+	}
 
-	void reset();
-
-	bool IsEmpty();
-
-	int HowManyItem();
-
-	// ban copy constructor and assignment operators
-	Stack(const Stack& rhs) = delete;
-	Stack& operator = (const Stack& rsh) = delete;
+	void Push(int element) {
+		try {
+			if (CurrentSize() < MaxSize()) {
+				mTop++;
+				mStack[mTop] = element;
+			}
+			else {
+				throw StackOverFlowExecption();
+			}
+		
+		}
+		catch (std::exception& e) {
+			cout << e.what();
+			exit(0); // need a better way to handle the execption		
+		}					
+	}
 	
+
+	void Pop() {
+	
+		try {
+			if (CurrentSize() <= MaxSize()) {
+				mTop--;
+								
+			}
+			else {
+				throw StackOverFlowExecption();
+			}
+
+		}
+		catch (std::exception& e) {
+			cout << e.what();
+			exit(0); // need a better way to handle the execption		
+		}
+	
+	}
+
+	//int Top();
+
+	//void reset();
+
+	//bool IsEmpty();
+
+	//int HowManyItem();
+
+	//// ban copy constructor and assignment operators
+	//Stack(const Stack& rhs) = delete;
+	//Stack& operator = (const Stack& rsh) = delete;
+
 };
+
 
