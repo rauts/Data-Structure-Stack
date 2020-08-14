790cce67898e8ef459b8d3e87a635f847959f9a8
Sunil Raut <sunil.rautkshetri@gmail.com>
2020-08-14 09:18:17 +0200

    Add header file StackOverFlowExecption.h

---
+++ Stack/StackOverFlowExecption.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <exception>
+
+class StackOverFlowExecption : public std::exception
+{
+	const char* what() const noexcept {
+		return "\nException: Stack Overflow\n";
+	}
+};
