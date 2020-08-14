ca2ee5d3f485057fd07ff37e38c2888f5a9b642d
Sunil Raut <sunil.rautkshetri@gmail.com>
2020-08-14 09:09:38 +0200

    Rename main.cpp file to Stack.cpp

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,12 +1,31 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
 #include "Stack.h"
 
+int main()
+{
+    Stack<int> s{ 5 };
+    s.Push(1);
+    s.Push(2);
+    s.Push(3);
+    cout << "s.Push(1)\n";
+    cout << "s.Push(2)\n";
+    cout << "s.Push(3)\n";
+    cout << "Current size = " << s.CurrentSize() << '\n';
+    s.Push(4);
+    s.Push(5);
+    cout << "s.Push(4)\n";
+    cout << "s.Push(5)\n";
 
-Stack::Stack(int maxStackSize) {
-	mMaxStackSize = maxStackSize;
-	mTop = new int[mMaxStackSize] {};
-}
 
-Stack::~Stack() {
-	delete[] mTop;
+    cout << "s.Push(6)\n";
+    s.Push(6);
+
+    return 0;
 }
 
